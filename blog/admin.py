3b390d11cc65659971b9585b92ef9358a61befef
Farov Amir <farov_a@iuca.kg>
2025-09-16 22:04:49 +0600

    blog

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-from django.db import models
-from django.contrib.auth.admin import UserAdmin
-
-
-admin.site.register(Post)   
-class GroupIsAdminClass(UserAdmin):
-    list_display = ["title", "content", "created_at"]
